d89487f9bd5296f596d7f94b17af3af4ad1b5816
unknown <jonferreira.psd@gmail.com>
2025-06-30 17:21:45 -0300

    manipulação do DOM e declaração de variaveis

---
+++ script.js
@@ -0,0 +1,10 @@
+const history = document.querySelector(".history");
+const result = document.querySelector(".result");
+const buttons = document.querySelector(".btn");
+const themeToggle = document.getElementById("Toogle-theme");
+const body = document.body;
+
+let history = "";
+let current = "0";
+let operator = "";
+let resetNext = false;
